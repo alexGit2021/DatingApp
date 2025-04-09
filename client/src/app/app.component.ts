7520f8b7812800acfa3d6e38d2797fc0f630c16f
Alex <alexandru.dragne@crystal-system.eu>
2025-04-09 16:12:30 +0300

    End of section 6 part 2

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
-import { HomeComponent } from "./home/home.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NavComponent, HomeComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, NavComponent],
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   private accountService = inject(AccountService);
